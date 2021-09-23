14c7dfa6e9946fef765bab3d701f0ec75e024750
friend <metalhead2112001@gmail.com>
2021-09-23 10:58:18 +0300

    vitu hyvä

--- OPTeht6B/Program.cs
+++ OPTeht6B/Program.cs
@@ -6,9 +6,26 @@ namespace OPTeht6B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anna lause:");
+            Console.Write("Anna lause: ");
             string tutkittava = Console.ReadLine();
+            //Metodin kutsu:
+            int tulos = LaskeVL(tutkittava);
+            string yksMoni = tulos == 1 ? "kappale" : "kappaletta";
+            Console.WriteLine("Välilyöntien lukumäärä lauseessa \"{0}\" on {1} {2}.", tutkittava, tulos, yksMoni);
 
+        }
+        //Määrittely
+        private static int LaskeVL(string str)
+        {
+            int lukumaara = 0;
+            string str1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                str1 = str.Substring(i, 1);
+                if (str1 == " ")
+                    lukumaara++;
+            }
+            return lukumaara;
         }
     }
 }
