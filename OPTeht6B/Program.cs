ed571113be6f2eb20ec9e915f69ef5748e01101c
friend <metalhead2112001@gmail.com>
2021-09-23 10:19:43 +0300

    Add project files.

---
+++ OPTeht6B/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OPTeht6B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna lause:");
+            string tutkittava = Console.ReadLine();
+
+        }
+    }
+}
